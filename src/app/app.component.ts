0f74e2e747fe358d8e649505a4117d915eeaf480
Raian Nicacio <raiannicacio@id.uff.br>
2019-06-28 17:20:38 -0300

    Auth Error at this point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import {
+ HttpClientModule,
+ HttpClient,
+ HTTP_INTERCEPTORS,
+} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  data: any =null;
+  constructor(private _http:HttpClient) {}
   title = 'angular-clash';
+ ngOnInit(){ return this._http
+    .get('http://api.clashofclans.com/v1/clans?locationId=aaaa')
+    .subscribe(data => {
+      this.data = data;
+      console.log(data);
+    });
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule} from '@angular/common/http'
+import { Interceptor } from './auth/interceptor.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    Interceptor,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
