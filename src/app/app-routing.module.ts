82004f2560044f5e8dae00d9dc2592cadb6bd3b5
Raian Nicacio <raiannicacio@id.uff.br>
2019-06-29 11:15:24 -0300

    Second try to upload to heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ListdetailsComponent } from './listdetails/listdetails.component';
+
+const routes: Routes = [
+  {path : 'player', component: ListdetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
